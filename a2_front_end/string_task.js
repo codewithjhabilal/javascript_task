c0702f76aca4daf794d2fb4564881cc1301d1339
codewithjhabilal <jhabilalbhandari48@gmail.com>
2025-09-25 18:51:46 +0545

    add string task

---
+++ a2_front_end/string_task.js
@@ -0,0 +1,87 @@
+// 1. Write a program that counts how many times the letter 'e' appears in the string `"Hello EveryOne"`
+
+// let a=`"Hello EveryOne"`
+// let count=0
+// for(let i=0;i<a.length;i++){
+//     if(a[i].toLowerCase()==='e'){
+//         count++
+
+//     }
+// }
+// console.log(count)
+
+// 2. Create a program that checks if a string starts with the letter 'H' and ends with the letter 'e'. Use `ifelse` statements.
+
+// let letter='helle'
+// if(letter.toLowerCase().startsWith('h') && letter.toLowerCase().endsWith('e')){
+//     console.log('leter startwith h and ends with letter e')
+// }else{
+//     console.log('not letter not start with h and end with e')
+// }
+
+// 3. Write a program that loops through a string and prints only the uppercase letters.
+// let randomWord='hello sipalaya'
+
+// for(let i=0;i<randomWord.length;i++){
+//     console.log(randomWord.toUpperCase())
+// }
+
+// 4. Using a loop, write a program that replaces every occurrence of the word "Hello" with "Hi" in a given
+// string.
+
+// let sentence = "Hello , Hello Sipalaya!"
+// let words = sentence.split(" ")
+// let newword= ""
+
+// for (let i = 0; i < words.length; i++){
+//     if (words[i] === "Hello"){
+//         newword+="Hi "
+//     }else{
+//         newword += words[i] + ""
+//     }
+// }
+
+// console.log(newword)
+
+// 5. Create a program that counts the number of vowels (a, e, i, o, u) in the string `"Hello EveryOne"`.
+
+// let word="Hello EveryOne"
+// let newword=0
+
+// for(let i=0;i<word.length;i++){
+//     let words=word[i].toLowerCase()
+//     if (words==='a' || words==='e' || words==='i' || words==='o'||words==='u'){
+
+//         newword++
+//     }
+   
+// }
+
+// console.log(newword)
+
+
+// 6. Write a program that uses a loop to concatenate the first letter of each word in the string `"Hello
+// EveryOne"` to form a new string.
+
+// let sentence = "Hello EveryOne"
+// let words = sentence.split(" ")
+// let newA = ""
+
+// for (let i = 0; i < words.length; i++){
+//     newA += words[i][0]  
+// }
+
+// console.log(newA)
+
+// 7. Create a program that checks if a string is a palindrome (reads the same backward as forward). Use `ifelse` statements.
+
+// let pal='mom'
+// let newPal=''
+// for(let i=pal.length-1;i>=0;i--){
+//     newPal=newPal+pal[i]
+// }
+// if(pal==newPal){
+//     console.log('pal')
+// }else{
+//     console.log('not pal')
+// }
