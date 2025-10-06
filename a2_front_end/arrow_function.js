98201e91893e887aae960fb860bb27993b4e70ba
codewithjhabilal <jhabilalbhandari48@gmail.com>
2025-10-06 20:00:37 +0545

    arrow function task add

---
+++ a2_front_end/arrow_function.js
@@ -0,0 +1,70 @@
+// 1.// Write an arrow function that returns the sum of two numbers
+
+// const add=(a,b)=>{
+//     return a+b
+// }
+// console.log(add(2,5))
+
+// 2.Write an arrow function that returns the square of a number.
+
+// const square=(a)=>{
+//     return a*a
+// }
+
+// console.log(square(4))
+
+// 3.Write an arrow function that checks if a number is even or odd
+
+// const check=(a)=>{
+//     if(a%2==0){
+//         return ("even")
+//     }else{
+//        return ("odd")
+//     }
+// }
+// console.log(check(4))
+// console.log(check(5))
+
+// 4.. Write an arrow function that returns the length of a given string.
+
+// const length=(a)=>{
+
+//     return a.length
+// }
+
+// console.log(length('hello'))
+
+// 5Write an arrow function that converts a string to uppercase
+
+// const upper=(a)=>{
+//     return a.toUpperCase()
+// }
+
+// console.log(upper("ram")) 
+
+// 6.Write an arrow function that concatenates two strings.
+
+// const strAdd=(a,b)=>{
+//     return a.concat(" ",b)
+// }
+
+// console.log(strAdd('ram','shyam'))
+
+
+
+// 8. Write an arrow function that checks if a number is positive, negative, or zero.
+
+// const checkNum = (n)=>{
+//     if (n > 0) {
+//         return "positive"
+//     } else if (n < 0) {
+//         return "negative"
+//     } else {
+//         return "zero"
+//     }
+// };
+
+// console.log(checkNum(5))  
+// console.log(checkNum(-3))  
+// console.log(checkNum(0))  
+
