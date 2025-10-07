2dccf4945de54d97f04af880909df09b11baa17b
codewithjhabilal <jhabilalbhandari48@gmail.com>
2025-10-07 20:09:28 +0545

    add arrow block function code modify

--- a2_front_end/arrow_function.js
+++ a2_front_end/arrow_function.js
@@ -87,7 +87,7 @@
 
 // const sumArray = arr => {
 //     let sum = 0
-//     for (let i=0; i<arr.length; i++) {  // length property use ho rahi hai
+//     for (let i=0; i<arr.length; i++) {  
 //         sum += arr[i]
 //     }
 //     return sum
